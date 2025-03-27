059d6724611c9d97727467915abc651fe23f9141
Aman9058490490 <sharmaman9058@gmail.com>
2025-03-27 14:33:44 +0530

    27-03-2025

--- kadans_algo_appr2.cpp
+++ kadans_algo_appr2.cpp
@@ -1,15 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int maxSubArray(vector<int>& nums){
-      int maxi=INT_MIN,sum=0;
+      int maxi=INT_MIN,sum=0,ansstart=-1,ansEnd=-1,start;
       for(int i=0;i<nums.size();i++){
+          if(sum==0){
+              start=i;
+            }
         sum=sum+nums[i];
         if(sum>maxi){
             maxi=sum;
+            ansstart=start;
+            ansEnd=i;
         }
         if(sum<0){
             sum=0;
         }
+      }
+      for(int j=ansstart;j<=ansEnd;j++){
+        cout<<nums[j]<<" ";
       }
       return maxi;
 }
